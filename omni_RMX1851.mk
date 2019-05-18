809e929e3ae5a0a995c6f2b2b9004359eaaabe55
chankruze <chankruze@gmail.com>
2019-05-18 11:32:44 +0530

    RMX1851: Update Tree
    * Add crypto & stock pie blobs
    * Override some default properties
    * Add debug support
    * Remove deprciated TW flags
    * Remove default init.usb.rc
    
    Signed-off-by: chankruze <chankruze@gmail.com>

--- omni_RMX1851.mk
+++ omni_RMX1851.mk
@@ -11,11 +11,22 @@ $(call inherit-product, vendor/omni/config/common.mk)
 PRODUCT_BRAND := Realme
 PRODUCT_DEVICE := RMX1851
 PRODUCT_MANUFACTURER := Realme
-PRODUCT_MODEL := RMX1851
+PRODUCT_MODEL := Realme 3 Pro
 PRODUCT_NAME := omni_RMX1851
 
 # enable stock zip packages flash
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+    ro.treble.enabled=true \
+    persist.sys.usb.config=mtp \
+    persist.service.adb.enable=1 \
+    persist.service.debuggable=1 \
     ro.secure=1 \
     ro.adb.secure=0 \
     ro.allow.mock.location=0
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_BRAND=Realme \
+    PRODUCT_DEVICE=RMX1851 \
+    PRODUCT_MANUFACTURER=Realme \
+    PRODUCT_MODEL=Realme 3 Pro \
+    PRODUCT_NAME=RMX1851
